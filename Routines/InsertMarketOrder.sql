c44b3cacce197dd641f29c090b3e70d1f8c279ff
Micael Dias <8200383@estg.ipp.pt>
2022-06-15 20:12:47 +0100

    Add Routine InsertMarketOrder

---
+++ Routines/InsertMarketOrder.sql
@@ -0,0 +1,27 @@
+CREATE PROCEDURE InsertMarketOrder @MarketCode VARCHAR, @Ingredient VARCHAR, @Quantity QuantityType AS
+
+DECLARE @MarketNo INT;
+
+SELECT @MarketNo = m.MarketNo
+FROM dbo.Market AS m
+WHERE m.MarketCode LIKE @MarketCode
+
+INSERT INTO dbo.MarketOrder (MarketNo)
+VALUES (@MarketNo)
+
+DECLARE @OrderNo INT;
+
+SELECT TOP 1 @OrderNo = m.OrderNo
+FROM dbo.MarketOrder AS m
+ORDER BY m.MarketNo DESC;
+
+DECLARE @IngredientNo INT;
+
+SELECT @IngredientNo = i.IngredientNo
+FROM dbo.Ingredient AS i
+WHERE i.IngredientName LIKE '%' + @Ingredient + '%'
+
+INSERT INTO dbo.MarketOrderIngredientDetail (OrderNo, IngredientNo, Quantity)
+VALUES (@OrderNo, @IngredientNo, @Quantity)
+
+-- Adicionar data no Market Order
